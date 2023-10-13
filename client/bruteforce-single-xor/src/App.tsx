52d4e78908714c4b9bcb29838f41d70da251b319
Bridge-PC <giovanespace@gmail.com>
2023-10-13 14:56:26 -0300

    iniciando projeto com client e server

---
+++ client/bruteforce-single-xor/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <h1>Bruteforce Single-byte Xor</h1>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ client/bruteforce-single-xor/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import './Main.css'
+
+function Main() {
+    return (
+        <>
+        <main>
+            <input type='text' placeholder='Input' />
+        </main>
+        <button>Ok</button>
+        </>
+    )
+}
+
+export default Main
