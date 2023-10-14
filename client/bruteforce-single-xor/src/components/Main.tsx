e701e502952674a7c937173e1d6b7fc822be618a
GiovanePS <giovanespace@gmail.com>
2023-10-14 02:30:30 -0300

    Removendo server, adicionando script e atualizações de interface

--- client/bruteforce-single-xor/src/components/Main.tsx
+++ client/bruteforce-single-xor/src/components/Main.tsx
@@ -1,12 +1,42 @@
+import {useState} from 'react'
 import './Main.css'
 
 function Main() {
+    
+    const [encodedString, setEncodedString] = useState("")
+    const [resultsList, setResultsList] = useState<string[]>([]);
+
+    const results = (input: string): void => {
+        try {
+            let results: string[] = []
+
+            let inputLenght: number = input.length
+            for (let i: number = 0; i < inputLenght; i += 2) {
+                results.push(String.fromCharCode(parseInt(input.slice(i, i+2), 16)))
+            }
+
+            setResultsList(prevList => [...prevList, ...results])
+        } catch (error) {
+            console.error((error as Error).message)
+        }
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        results(encodedString)
+    }
+    
     return (
         <>
-        <main>
-            <input type='text' placeholder='Input' />
-        </main>
-        <button>Ok</button>
+        <form onSubmit={handleSubmit}>
+            <main>
+                <input type='text' placeholder='Input' value={encodedString} onChange={e => setEncodedString(e.target.value)} />
+                {resultsList.map((result, index) => (
+                    <div key={index}>{result}</div>
+                ))}
+            </main>
+            <button>Ok</button>
+        </form>
         </>
     )
 }
